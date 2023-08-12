5623f1db19b2971789ac36e9c3aeb1f1b10e09f6
rohithvankadaru <rohithvankadaru405@gmail.com>
2023-08-12 21:24:03 +0530

    encanhced skill section by adding 3 buttons

--- logic.js
+++ logic.js
@@ -10,3 +10,49 @@ function changeText() {
         developerText.innerHTML = "Frontend Developer"
     }
 }
+
+
+let allBtn = document.querySelector("#All");
+let frontendBtn = document.querySelector("#Frontend");
+let backendBtn = document.querySelector("#Backend");
+
+let allComponents = document.querySelectorAll(".All");
+let frontendComponents = document.querySelectorAll(".Frontend");
+let backendComponents = document.querySelectorAll(".Backend");
+
+
+allBtn.addEventListener("click", ()=>{
+    allComponents.forEach(element => {
+        element.style.display = "flex";
+    });
+    backendComponents.forEach(element => {
+        element.style.display = "flex";
+    });
+    frontendComponents.forEach(element => {
+        element.style.display = "flex";
+    });
+});
+
+frontendBtn.addEventListener("click", ()=>{
+    allComponents.forEach(element => {
+        element.style.display = "none";
+    });
+    backendComponents.forEach(element => {
+        element.style.display = "none";
+    });
+    frontendComponents.forEach(element => {
+        element.style.display = "flex";
+    });
+});
+
+backendBtn.addEventListener("click", ()=>{
+    allComponents.forEach(element => {
+        element.style.display = "none";
+    });
+    frontendComponents.forEach(element => {
+        element.style.display = "none";
+    });
+    backendComponents.forEach(element => {
+        element.style.display = "flex";
+    });
+});
